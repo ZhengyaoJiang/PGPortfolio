2b361a17fa47eea80de75485d094a4e31f00c966
dan <daniel.lacombe.jr@gmail.com>
2018-02-11 14:02:34 -0500

    changing some tensorboard names

--- pgportfolio/learn/network.py
+++ pgportfolio/learn/network.py
@@ -114,13 +114,13 @@ class CNN(NeuralNetWork):
                 #btc_bias = tf.zeros((self.input_num, 1))
                 btc_bias = tf.get_variable("btc_bias", [1, 1], dtype=tf.float32,
                                        initializer=tf.zeros_initializer)
-                self.add_layer_to_dict(layer["type"], network, weights=False)
+                # self.add_layer_to_dict(layer["type"], network, weights=False)
                 btc_bias = tf.tile(btc_bias, [self.input_num, 1])
                 network = tf.concat([btc_bias, network], 1)
                 self.voting = network
                 self.add_layer_to_dict('voting', network, weights=False)
                 network = tflearn.layers.core.activation(network, activation="softmax")
-                self.add_layer_to_dict(layer["type"], network, weights=False)
+                self.add_layer_to_dict('softmax_layer', network, weights=False)
 
             elif layer["type"] == "EIIE_LSTM" or\
                             layer["type"] == "EIIE_RNN":
